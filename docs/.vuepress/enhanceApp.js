b1bf15fafbc500a924f2fbde17a3894bca4ae875
悠悠狮羝LumiCornX <50665778+LumiCorn@users.noreply.github.com>
2025-03-06 23:13:33 +0800

    更新 enhanceApp.js

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -8,4 +8,25 @@ export default ({
   // window.Vue = vue // 使页面中可以使用Vue构造函数 （使页面中的vue demo生效）
 }
 
+import katex from "katex";
 import "katex/dist/katex.min.css";
+
+export default ({ router }) => {
+  router.afterEach(() => {
+    setTimeout(() => {
+      document.querySelectorAll("span.katex-mathml").forEach((el) => {
+        el.style.display = "inline";
+      });
+
+      // 强制渲染行内公式
+      document.querySelectorAll("p, li, span").forEach((el) => {
+        el.innerHTML = el.innerHTML.replace(/\$(.+?)\$/g, (match, math) => {
+          return katex.renderToString(math, {
+            throwOnError: false,
+            displayMode: false, // 强制行内显示
+          });
+        });
+      });
+    }, 500);
+  });
+};
